3cad64751ef81ecc449b038b8be4458d51593359
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-09 19:48:31 -0500

    save initial files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log("I'm linked");
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) =>{
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"},{name: "All Items"},{name: "New Item"}];
+});
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof";
+});
