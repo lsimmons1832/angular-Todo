08fe9c4fcc3525fe06c33b130275b49a95a91d16
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-13 11:43:16 -0500

    pulling in data from firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,40 +1,51 @@
-var app = angular.module("TodoApp", []);
+app.run((FIREBASE_CONFIG) =>{
+ firebase.initializeApp(FIREBASE_CONFIG);
+});
 
 app.controller("NavCtrl", ($scope) =>{
 	$scope.cat = "Meow";
 	$scope.navItems = [{name: "Logout"},{name: "All Items"},{name: "New Item"}];
 });
 
-app.controller("ItemCtrl", ($scope) => {
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 	$scope.dog = "Woof";
 	$scope.showListView = true;
-  $scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          assignedTo: "Callan",
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          assignedTo: "Lauren",
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          assignedTo: "Zoe",
-        }
-      ];
+  $scope.items = [];
+
 	$scope.newItem = () => {
 		$scope.showListView = false;
 	};
 
-		$scope.allItems = () => {
+	$scope.allItems = () => {
 			$scope.showListView = true;
 	};
+
+	let getItemList = () =>{  //This is the function getting info from Firebase
+		let itemz = []; //This itemz array only exist in this function
+		return $q((resolve, reject) =>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+			.then((fbItems)=>{
+          var itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+			}).catch((error) =>{
+				reject(error);
+			});
+		});
+	};
+
+		let getItems = () => {
+			getItemList().then((itemz) =>{
+				$scope.items = itemz;
+			}).catch((error) =>{
+				console.log("get Error", error);
+			});
+		};
+
+		getItems();
 
 
 });
