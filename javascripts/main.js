298fa43b57e0aa1b128c6e96c41842b13bb4e9b0
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-13 12:16:23 -0500

    adding new task to firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -47,5 +47,26 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
 		getItems();
 
+		let postNewItem = (newItem) =>{
+			return $q ((resolve, reject) =>{
+				$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+				.then((resultz) =>{
+					resolve(resultz);
+				}).catch((error) => {
+					reject(error);
+				});
+			});
+		};
+
+		$scope.addNewItem = () => {
+			$scope.newTask.isCompleted = false;
+			postNewItem($scope.newTask).then((response) =>{
+				$scope.newTask = {};
+				$scope.showListView = true;
+				getItems();
+			}).catch((error) => {
+				console.log("Add error", error);
+			});
+		};
 
 });
