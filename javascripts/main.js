b4f52de3c0dfebeb9e2f26b9d3ce3e1f39a0c56e
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-15 20:30:58 -0500

    weirdness

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,50 @@
-app.run((FIREBASE_CONFIG) =>{
+app.run(function(FIREBASE_CONFIG){
  firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("NavCtrl", ($scope) =>{
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/items/list', {
+			templateUrl:'partials/item-list.html',
+			controller: 'ItemListCtrl'
+		})
+		.when('/items/new', {
+			templateUrl:'partials/item-new.html',
+			controller: 'ItemNewCtrl'
+		})
+		.when('/item/view/:id', {
+			templateUrl: 'partials/item-view.html',
+			controller: 'ItemViewCtrl'
+		})
+		.when('/item/edit/:id', {
+			templateUrl:'partials/item-new.html',
+			controller: 'ItemEditCtrl'
+		})
+		.otherwise('/items/list');
+});
+
+app.controller("NavCtrl", function ($scope) {
 	$scope.cat = "Meow";
 	$scope.navItems = [{name: "Logout"},{name: "All Items"},{name: "New Item"}];
 });
 
-app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+app.controller("ItemListCtrl", function () {
+console.log("inside ItemListCtrl");
+});
+
+app.controller("ItemNewCtrl", function () {
+console.log("inside ItemNewCtrl");
+});
+
+app.controller("ItemViewCtrl", function () {
+console.log("inside ItemViewCtrl");
+});
+
+app.controller("ItemEditCtrl", function () {
+console.log("inside ItemEditCtrl");
+});
+
+app.controller("ItemCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
 	$scope.dog = "Woof";
 	$scope.showListView = true;
   $scope.items = [];
