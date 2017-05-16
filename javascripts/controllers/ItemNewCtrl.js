e034abdda80fb43aac16dabeb439b00d7bd32912
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-15 21:16:53 -0500

    removed main.js

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,3 +1,23 @@
-app.controller("ItemNewCtrl", function () {
-console.log("inside ItemNewCtrl");
+app.controller("ItemNewCtrl", function ($http, $q, $scope, FIREBASE_CONFIG) {
+		let postNewItem = (newItem) =>{
+			return $q ((resolve, reject) =>{
+				$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+				.then((resultz) =>{
+					resolve(resultz);
+				}).catch((error) => {
+					reject(error);
+				});
+			});
+		};
+
+		$scope.addNewItem = () => {
+			$scope.newTask.isCompleted = false;
+			postNewItem($scope.newTask).then((response) =>{
+				$scope.newTask = {};
+				// getItems(); we don't need this we just need to switch views
+			}).catch((error) => {
+				console.log("Add error", error);
+			});
+		};
+
 });
