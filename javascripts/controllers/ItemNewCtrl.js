03f9cf0c7679b76005e93a5eebf8e220ff5648f0
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-22 20:55:00 -0500

    completed Auth

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,8 +1,9 @@
-app.controller("ItemNewCtrl", function ($http, $location, $q, $scope, FIREBASE_CONFIG, ItemFactory) {
+app.controller("ItemNewCtrl", function ($rootScope, $http, $location, $q, $scope, FIREBASE_CONFIG, ItemFactory) {
 
 
 		$scope.addNewItem = () => {
 			$scope.newTask.isCompleted = false;
+			$scope.newTask.uid = $rootScope.user.uid;
 			ItemFactory.postNewItem($scope.newTask).then((response) =>{
 				$scope.newTask = {};
 				$location.url("/items/list");
