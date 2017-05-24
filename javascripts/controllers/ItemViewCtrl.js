aa84c6059afaaf6d572537084177f97d4dde0e31
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-23 21:40:51 -0500

    save work

--- javascripts/controllers/ItemViewCtrl.js
+++ javascripts/controllers/ItemViewCtrl.js
@@ -1,11 +1,19 @@
-	app.controller("ItemViewCtrl", function ($routeParams, $scope, ItemFactory) {
+	app.controller("ItemViewCtrl", function ($routeParams, $scope, ItemFactory, ToolFactory) {
 	$scope.selectedItem = {};
+	$scope.tools = [];
 
 	ItemFactory.getSingleItem($routeParams.id)
 	.then((results) =>{
 		$scope.selectedItem = results.data;
 	}).catch((error) => {
 		console.log("getSingleItem error", error);
+	});
+
+	ToolFactory.getToolList($routeParams.id).then((results) =>{
+		console.log("results", results);
+		$scope.tools = results;
+	}).catch((error)=>{
+		console.log("error in getToolList", error);
 	});
 
 });
